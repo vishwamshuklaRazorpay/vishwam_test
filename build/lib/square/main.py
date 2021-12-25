165649467e242f01e7abda126b4c96728454fe35
Vishwam Shukla <shuklavishwam@gmail.com>
2021-12-25 18:42:54 +0530

    add

---
+++ build/lib/square/main.py
@@ -0,0 +1,8 @@
+def main():
+    number = int(input("Enter number (Only positive integer is allowed)"))
+    print(f'{number} square is {number ** 2}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -9,29 +9,21 @@ setup(
     description="squares the number",
     long_description=readme_content,
     long_description_content_type='text/markdown',
-    url="https://github.com/razorpay/razorpay-python",
-    author="Team Razorpay",
+    url="https://github.com/vishwamshuklaRazorpay/vishwam_test",
+    author="Vishwam",
     license="MIT",
-    install_requires=["requests"],
-    include_package_data=True,
-    package_dir={'razorpay': 'razorpay', 'razorpay.resources': 'razorpay/resources'},
-    packages=['razorpay', 'razorpay.resources'],
-    keywords='razorpay payment gateway india',
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-
-        # List of supported Python versions
-        # Make sure that this is reflected in .travis.yml as well
-        "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
+            "License :: OSI Approved :: MIT License",
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+        ],
+    packages=["square"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts":[
+            "square=square.main:main",
+        ]
+    },
 )
